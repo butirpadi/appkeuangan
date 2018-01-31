1b56086387e6b8c23ddb056432045b465320075e
butirpadi <butirpadi@gmail.com>
2018-01-31 14:17:12 +0700

    Commit emboh

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ return array(
 		'mysql' => array(
 			'driver'   => 'mysql',
 			'host'     => 'localhost',
-			'database' => 'appsek',
+			'database' => 'simasad',
 			'username' => 'root',
 			'password' => 'root',
 			'charset'  => 'utf8',
